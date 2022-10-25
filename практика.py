127a58ee762ffde5b7d92a6fe0f217f94ba30226
MariaZborouskaia <marysi4ka4@mail.ru>
2022-10-25 14:35:42 +0300

    ok

---
+++ практика.py
@@ -0,0 +1,19 @@
+per1 = "Hi" #str
+print(per1)
+per2 = 34 #int
+print(per2)
+per3 = 23.56 #floot
+print(per3)
+per4 = None
+print(per4)
+per5 = True
+print(per5)
+per6 = False
+print(per6)
+per7 = "Server"
+print(per7)
+per8 = 1
+print(per8)
+per9 = 3.5
+print(per9)
+
